4bf5ef7e2b2433e0240986c9e779cca6a3f5876e
Erik Pique <erik.pique@newshore.es>
2017-09-09 12:44:07 +0200

    master - refactor add base type for EventAggregate

---
+++ EventAggregate.Core/Implementation/Base/EventAggregateBase.cs
@@ -0,0 +1,45 @@
+﻿using EventAggregate.Core.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventAggregate.Core.Implementation.Base
+{
+    public abstract class EventAggregateBase : IEventAggregate
+    {
+        private readonly IDictionary<Type, List<WeakReference>> _eventAggregates;
+
+        protected EventAggregateBase()
+        {
+            _eventAggregates = new ConcurrentDictionary<Type, List<WeakReference>>();
+        }
+
+        public abstract void Publish<TEvent>(TEvent @event);
+
+        public void Subscriber(object subject)
+        {
+            var interfaceTypes = subject.GetType().GetInterfaces()
+                .Where(@interface => @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(ISubscriber<>));
+
+            var weakReference = new WeakReference(subject);
+
+            foreach (var interfaceType in interfaceTypes)
+            {
+                var subscribers = GetSubscribes(interfaceType);
+                subscribers.Add(weakReference);
+            }
+        }
+
+        protected List<WeakReference> GetSubscribes(Type interfaceType)
+        {
+            if (!_eventAggregates.TryGetValue(interfaceType, out List<WeakReference> subscribers))
+            {
+                subscribers = new List<WeakReference>();
+                _eventAggregates.Add(interfaceType, subscribers);
+            }
+
+            return subscribers;
+        }
+    }
+}
--- EventAggregate.Core/Implementation/EventAggregate.cs
+++ EventAggregate.Core/Implementation/EventAggregate.cs
@@ -1,22 +1,15 @@
-﻿using EventAggregate.Core.Interfaces;
+﻿using EventAggregate.Core.Implementation.Base;
+using EventAggregate.Core.Interfaces;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
 namespace EventAggregate.Core.Implementation
 {
-    public sealed class EventAggregate : IEventAggregate
+    public sealed class EventAggregate : EventAggregateBase
     {
-        private readonly IDictionary<Type, List<WeakReference>> _eventAggregates;
-
-        public EventAggregate()
-        {
-            _eventAggregates = new ConcurrentDictionary<Type, List<WeakReference>>();
-        }
-
-        public void Publish<TEvent>(TEvent @event)
+        public override void Publish<TEvent>(TEvent @event)
         {
             var subscribeType = typeof(ISubscriber<>).MakeGenericType(typeof(TEvent));
             var subscribersToRemove = new List<WeakReference>();
@@ -39,33 +32,6 @@ namespace EventAggregate.Core.Implementation
             {
                 subscribersToRemove.ForEach(subscriber => subscribers.Remove(subscriber));
             }
-        }
-
-        public void Subscriber(object subject)
-        {
-            var interfaceTypes = subject.GetType().GetInterfaces()
-                .Where(@interface => @interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(ISubscriber<>));
-
-            var weakReference = new WeakReference(subject);
-
-            foreach (var interfaceType in interfaceTypes)
-            {
-                var subscribers = GetSubscribes(interfaceType);
-                subscribers.Add(weakReference);
-            }
-        }
-
-        private List<WeakReference> GetSubscribes(Type interfaceType)
-        {
-            List<WeakReference> subscribers;
-
-            if (!_eventAggregates.TryGetValue(interfaceType, out subscribers))
-            {
-                subscribers = new List<WeakReference>();
-                _eventAggregates.Add(interfaceType, subscribers);
-            }
-
-            return subscribers;
         }
     }
 }
